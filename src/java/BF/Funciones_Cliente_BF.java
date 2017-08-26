f9fed794e1444e71099937aff22f453969756e3a
fabian <fabian@fabian-PC>
2017-08-26 17:59:48 -0500

    Ajustes con reporte contabless

--- src/java/BF/Funciones_Cliente_BF.java
+++ src/java/BF/Funciones_Cliente_BF.java
@@ -37,15 +37,7 @@ public class Funciones_Cliente_BF extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Funciones_Cliente</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Funciones_Cliente at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
         }
     }
 
